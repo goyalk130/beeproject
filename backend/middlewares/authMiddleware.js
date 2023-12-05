e2bcb2fde4f4ab40a7219daf0b9ea8a9ce718983
goyalk130 <goyalkaran130@gmail.com>
2023-12-05 12:30:49 +0530

    init

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,4 @@
+module.exports = (req,res,next)=>{
+        const data = req.body
+        console.log(data)
+}
---
+++ backend/routes/blogRoute.js
@@ -0,0 +1,7 @@
+const express = require("express")
+
+const blogRouter  = express.Router()
+
+blogRouter.use()
+
+module.exports = blogRouter
